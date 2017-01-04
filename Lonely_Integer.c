43b161526760c94e9b8e97570089ac3e80789a3b
sharadbhat <sharad.mbhat@gmail.com>
2017-01-05 02:59:19 +0530

    Hackerrank
    https://www.hackerrank.com/challenges/ctci-lonely-integer

---
+++ Lonely_Integer.c
@@ -0,0 +1,31 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main()
+{
+    int a[1000] = {0};
+    int i, n, b, biggest = 0;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &b);
+        if(b > biggest)
+        {
+            biggest = b;
+        }
+        a[b]++;
+    }
+    for(i = 1; i <= biggest; i++)
+    {
+        if(a[i] == 1)
+        {
+            printf("%d", i);
+            break;
+        }
+    }
+}
