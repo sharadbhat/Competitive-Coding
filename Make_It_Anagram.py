909424a06585ece53aaae989005afa418ec613b3
Sharad Bhat <sharad.mbhat@gmail.com>
2018-04-09 23:42:48 +0530

    Added make it anagram

---
+++ Make_It_Anagram.py
@@ -0,0 +1,10 @@
+# HACKERRANK
+# https://www.hackerrank.com/challenges/make-it-anagram-mglines/problem
+
+w1 = raw_input()
+w2 = raw_input()
+
+total = 0
+for letter in "abcdefghijklmnopqrstuvwxyz":
+    total += abs(w1.count(letter) - w2.count(letter))
+print total
