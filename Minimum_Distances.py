0faf2acb467b47eea780ecdbc9f0d15f5e5b69c3
Sharad Bhat <sharad.mbhat@gmail.com>
2018-04-11 00:47:25 +0530

    Added minimum distances

---
+++ Minimum_Distances.py
@@ -0,0 +1,24 @@
+# HACKERRANK
+# https://www.hackerrank.com/challenges/minimum-distances/problem
+
+import sys
+
+def minimumDistances(a):
+    d = {}
+    minimum = len(a)
+    for i in range(len(a)):
+        if a[i] in d:
+            dist = abs(d[a[i]] - i)
+            if minimum > dist:
+                minimum = dist
+        d[a[i]] = i
+    if minimum == len(a):
+        return -1
+    else:
+        return minimum
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    a = list(map(int, input().strip().split(' ')))
+    result = minimumDistances(a)
+    print(result)
