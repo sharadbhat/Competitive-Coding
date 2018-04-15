a928cda28db524ac7d4fcbd12a59290278602201
Sharad Bhat <sharad.mbhat@gmail.com>
2018-04-15 17:54:49 +0530

    Added priyanka and toys

---
+++ Priyanka_And_Toys.py
@@ -0,0 +1,21 @@
+# HACKERANK
+# https://www.hackerrank.com/challenges/priyanka-and-toys/problem
+
+import sys
+
+def toys(w):
+    w.sort()
+    count = 0
+    while len(w) > 0:
+        count += 1
+        maximum = w[0] + 4
+        delete = [i for i in w if i <= maximum]
+        for i in delete:
+            w.remove(i)
+    return count
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    w = list(map(int, input().strip().split(' ')))
+    result = toys(w)
+    print(result)
