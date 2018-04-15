fc366ca59ab9cadf27a40b22b6fc591e8698764b
Sharad Bhat <sharad.mbhat@gmail.com>
2018-04-15 17:54:57 +0530

    Added sherlock and array

---
+++ Sherlock_And_Array.py
@@ -0,0 +1,27 @@
+# HACKERANK
+# https://www.hackerrank.com/challenges/sherlock-and-array/problem
+
+import sys
+
+def solve(a):
+    if len(a) == 1:
+        return "YES"
+    left = sum(a[:0])
+    right = sum(a[1:])
+    curr = a[0]
+    next_ele = 1
+    while next_ele < len(a):
+        if left == right:
+            return "YES"
+        left += curr
+        right -= a[next_ele]
+        curr = a[next_ele]
+        next_ele += 1
+    return "NO"
+
+T = int(input().strip())
+for a0 in range(T):
+    n = int(input().strip())
+    a = list(map(int, input().strip().split(' ')))
+    result = solve(a)
+    print(result)
