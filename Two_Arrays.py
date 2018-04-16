2a5e20ee4857316dc03e181c5fa76488cfc476ee
Sharad Bhat <sharad.mbhat@gmail.com>
2018-04-16 23:51:38 +0530

    Added two arrays

---
+++ Two_Arrays.py
@@ -0,0 +1,25 @@
+# HACKERANK
+# https://www.hackerrank.com/challenges/two-arrays/problem
+
+import sys
+
+def twoArrays(k, A, B):
+    A.sort()
+    B.sort(reverse=True)
+    count = 0
+    i = 0
+    while i < len(A):
+        if A[i] + B[i] < k:
+            return "NO"
+        i += 1
+    return "YES"
+
+if __name__ == "__main__":
+    q = int(input().strip())
+    for a0 in range(q):
+        n, k = input().strip().split(' ')
+        n, k = [int(n), int(k)]
+        A = list(map(int, input().strip().split(' ')))
+        B = list(map(int, input().strip().split(' ')))
+        result = twoArrays(k, A, B)
+        print(result)
