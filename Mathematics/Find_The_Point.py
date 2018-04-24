2cfc223dd52ec8086c4eaaaa1f9281535be01d3e
Sharad Bhat <sharad.mbhat@gmail.com>
2018-04-24 23:08:23 +0530

    Added find the point

---
+++ Mathematics/Find_The_Point.py
@@ -0,0 +1,31 @@
+# HACKERRANK
+# https://www.hackerrank.com/challenges/find-point/problem
+
+import os
+import sys
+
+def findPoint(px, py, qx, qy):
+    return [qx+(qx-px), qy+(qy-py)]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    for n_itr in range(n):
+        pxPyQxQy = input().split()
+
+        px = int(pxPyQxQy[0])
+
+        py = int(pxPyQxQy[1])
+
+        qx = int(pxPyQxQy[2])
+
+        qy = int(pxPyQxQy[3])
+
+        result = findPoint(px, py, qx, qy)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
