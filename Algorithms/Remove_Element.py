9a666eec4c25bff780ed4acf4f5b6e64be690e2e
Sharad Bhat <sharad.mbhat@gmail.com>
2018-06-04 23:48:41 +0530

    Added remove element

---
+++ Algorithms/Remove_Element.py
@@ -0,0 +1,16 @@
+# LeetCode
+# https://leetcode.com/problems/remove-element/description/
+
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                del nums[i]
+            else:
+                i += 1
