07d62dda1742f531045bb09b5c4781cd347f303d
Sharad Bhat <sharad.mbhat@gmail.com>
2018-06-09 03:13:01 +0530

    Added number of 1 bits

---
+++ LeetCode/Number_Of_1_Bits.py
@@ -0,0 +1,15 @@
+# LeetCode
+# https://leetcode.com/problems/number-of-1-bits/description/
+
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while n != 0:
+            if n & 1:
+                count += 1
+            n = n >> 1
+        return count
