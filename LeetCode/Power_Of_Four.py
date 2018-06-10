658d4daa00d50c620cef85242f7c17965b2c9c03
Sharad Bhat <sharad.mbhat@gmail.com>
2018-06-10 17:39:43 +0530

    Added power of four

---
+++ LeetCode/Power_Of_Four.py
@@ -0,0 +1,14 @@
+# LeetCode
+# https://leetcode.com/problems/power-of-four/description/
+
+import math
+
+class Solution(object):
+    def isPowerOfFour(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+        return num == 4 ** round(math.log(num, 4))
