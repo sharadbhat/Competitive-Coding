4f9ee4b605eb6c858cfde6b6c4ac348c47e171a5
sharadbhat <sharad.mbhat@gmail.com>
2020-10-22 02:40:26 +0530

    better soln

--- LeetCode/Hamming_Distance.py
+++ LeetCode/Hamming_Distance.py
@@ -4,9 +4,8 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
         count = 0
-        while (x > 0 or y > 0):
-            if x % 2 != y % 2:
-                count += 1
+        x = x ^ y
+        while (x):
+            count += 1 if x & 1 else 0
             x = x >> 1
-            y = y >> 1
         return count
