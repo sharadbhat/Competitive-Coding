d4f0f0df80b8d73e8cdd510ccfd567da2fb06798
sharadbhat <sharad.mbhat@gmail.com>
2021-04-30 16:36:04 +0530

    change

--- LeetCode/Implement_strStr.py
+++ LeetCode/Implement_strStr.py
@@ -1,15 +1,20 @@
 # LeetCode
-#https://leetcode.com/problems/implement-strstr/description/
+# https://leetcode.com/problems/implement-strstr/
+
 
 class Solution:
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        if needle == "":
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not len(needle):
             return 0
-        if needle in haystack:
-            return haystack.index(needle)
+        if not len(haystack):
+            return -1
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i] == needle[0]:
+                found = True
+                for j in range(len(needle)):
+                    if needle[j] != haystack[i + j]:
+                        found = False
+                        break
+                if found:
+                    return i
         return -1
